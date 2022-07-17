7b8a30e48a50095cd8776ff0a16400a751f45650
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-07-17 16:04:18 -0500

    commit completed first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers  = (x) => x.slice(0,2);
+const returnLastTwoDrivers  = (x)=>x.slice(2,5);
+
+
+const selectDifferentDrivers = (arrayOfDrivers, driversToReturn) => {
+    return driversToReturn(arrayOfDrivers)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (x) => {
+    return function(fare) { return x * fare}
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+
